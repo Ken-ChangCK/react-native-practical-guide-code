7956a6d89892f950313a881adf125fc92677d71e
Ken Chang <rockkcor75@gmail.com>
2022-06-05 22:56:44 +0800

    feat: add drawer project

--- App.js
+++ App.js
@@ -1,81 +1,58 @@
-import { useState } from "react";
-import { StyleSheet, View, FlatList, Button } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [courseGoal, setCourseGoal] = useState([]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const startAddGoalHandler = () => {
-    setIsModalVisible(true);
-  };
-  const endAddGoalHandler = () => {
-    setIsModalVisible(false);
-  };
-
-  const addGoalHandler = (enterGoalText) => {
-    setCourseGoal((preValue) => [
-      ...preValue,
-      { text: enterGoalText, id: Math.random().toString() },
-    ]);
-    endAddGoalHandler();
-  };
-
-  const deleteGoalHandle = (id) => {
-    setCourseGoal((cuurrentCourseGoal) => {
-      return cuurrentCourseGoal.filter((goal) => goal.id !== id);
-    });
-  };
-
   return (
-    <>
-      <StatusBar style="light" />
-      <View style={styles.appContainer}>
-        <Button
-          title="add new goal"
-          color="#a065ec"
-          onPress={startAddGoalHandler}
-        ></Button>
-        {isModalVisible && (
-          <>
-            <GoalInput
-              onAddGoal={addGoalHandler}
-              onCancel={endAddGoalHandler}
-              visible={isModalVisible}
-            />
-          </>
-        )}
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoal}
-            keyExtractor={(item, index) => {
-              return item.id;
-            }}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  text={itemData.item.text}
-                  id={itemData.item.id}
-                  onDeleteItem={deleteGoalHandle}
-                />
-              );
-            }}
-          ></FlatList>
-        </View>
-      </View>
-    </>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          tabBarActiveTintColor: "#3c0a6b",
+          // drawerActiveBackgroundColor: "#f0e1ff",
+          // drawerActiveTintColor: "#3c0a6b",
+          // drawerStyle: {
+          //   backgroundColor: "#ccc",
+          // },
+        }}
+      >
+        <Tab.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+          // options={{
+          //   drawerLabel: "Welcome Screen-1",
+          //   drawerIcon: ({ color, size }) => (
+          //     <Ionicons name="home" color={color} size={size} />
+          //   ),
+          // }}
+        />
+        <Tab.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+          // options={{
+          //   drawerIcon: ({ color, size }) => (
+          //     <Ionicons name="person" color={color} size={size} />
+          //   ),
+          // }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-  },
-  goalsContainer: {
-    flex: 5,
-  },
-});
